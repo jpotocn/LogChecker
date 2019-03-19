9e2d831291e2cb1eea07504cdd00e65c3253ad9f
janp <jan.potocnik@solvera-lynx.com>
2019-03-19 16:27:36 +0100

    [GLC-003] added logback& fixing props file

--- src/main/java/com/solveralynx/logchecker/GemaLogChecker.java
+++ src/main/java/com/solveralynx/logchecker/GemaLogChecker.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 
 public class GemaLogChecker {
 
-    private static final Logger logger = LoggerFactory.getLogger("gema-logchecker");
+    private static final Logger logger = LoggerFactory.getLogger("logchecker");
     private static final String LOG_CHECKER_PROPERTIES = "logchecker.properties";
     private Properties properties = GemaLogChecker.getProperties(LOG_CHECKER_PROPERTIES);
 
@@ -37,20 +37,18 @@ public class GemaLogChecker {
         if (properties == null) {
             System.exit(3);
         }
-        final String filePath = properties.getProperty("filePath");
+        final String dirPath = properties.getProperty("dirPath");
         String specifiedText = "FullAjaxExceptionHandler";
         try {
-            File fileDir = FileUtils.getFile(getLastModified(filePath));
-            logger.warn("Checking log: " + fileDir);
+            File fileDir = FileUtils.getFile(getLastModified(dirPath));
+            logger.info("Reading last modified file..................... " + "(" + fileDir + ")");
             Scanner scanner = new Scanner(fileDir);
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 if (line.contains(specifiedText)) {
-                    logger.warn(specifiedText + " found in a log: " + fileDir + ".Sending an email...");
+                    logger.error(specifiedText + " found in a log:...................." + fileDir + ".Sending an email...");
                     //sendEmail();
                     break;
-                } else {
-                    logger.info(specifiedText + " not found in this line.");
                 }
             }
         } catch (Exception ex) {
@@ -61,8 +59,8 @@ public class GemaLogChecker {
     private static File getLastModified(String dirPath) {
         File lastModifiedFile = null;
         try {
-            File dir = new File(dirPath);
-            File[] files = dir.listFiles();
+            File directory = new File(dirPath);
+            File[] files = directory.listFiles();
             if (files == null || files.length == 0) {
                 return null;
             }
@@ -70,6 +68,7 @@ public class GemaLogChecker {
             lastModifiedFile = files[0];
             for (int i = 1; i < files.length; i++) {
                 if (lastModifiedFile.lastModified() < files[i].lastModified()) {
+                    logger.info("Pulling last modified file from............." + "(" + dirPath + ")");
                     lastModifiedFile = files[i];
                 }
             }
@@ -79,37 +78,9 @@ public class GemaLogChecker {
         return lastModifiedFile;
     }
 
-    private void sendEmail() throws MessagingException {
-        //TODO: Disabled because of an error.
+    public void sendEmail() throws MessagingException {
+
     }
-    //    if (properties == null) {
-    //        System.exit(1);
-    //    }
-    //
-    //    final String to = properties.getProperty("emailTo");
-    //    final String from = properties.getProperty("emailFrom");
-    //    final String subject = properties.getProperty("emailSubject");
-    //    final String description = properties.getProperty("emailDescription");
-    //    final String host = properties.getProperty("host");
-    //
-    //    try {
-    //        Properties properties = new Properties();
-    //        properties.setProperty("mail.host", host);
-    //        Session session = Session.getDefaultInstance(properties);
-    //        session = Session.getDefaultInstance(properties, null);
-    //        MimeMessage mimeMessage = new MimeMessage(session);
-    //        mimeMessage.setFrom(new InternetAddress(from));
-    //        mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-    //        mimeMessage.setSubject(subject);
-    //        mimeMessage.setDescription(description);
-    //
-    //        Transport.send(mimeMessage);
-    //
-    //        logger.info("Send message successfully!");
-    //    } catch (SendFailedException e) {
-    //        e.printStackTrace();
-    //    }
-    //}
 
     public static void main(String[] args) {
         new GemaLogChecker().findPattern();
